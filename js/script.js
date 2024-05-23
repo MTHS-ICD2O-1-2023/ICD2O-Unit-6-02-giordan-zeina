631cc51a1a092fb96e8c723c3fa2c74f05b2f72b
Giordan Zeina <144264315+giordan-zeina@users.noreply.github.com>
2024-05-23 18:19:42 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Giordan Zeina All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Giordan Zeina
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myCookies() {
+  if (localStorage.clicks) {
+    localStorage.clicks = Number(localStorage.clicks)
+  } else {
+    localStorage.clicks = 1
+  }
+  document.getElementById("clicks").innerHTML = localStorage.clicks
+}
+
+function myButtonClicked() {
+  localStorage.clicks = Number(localStorage.clicks) + 1
+
+  document.getElementById("clicks").innerHTML = localStorage.clicks
+}
